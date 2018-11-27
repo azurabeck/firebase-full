a7f1a2e3933e23ad3333de734be7aad3a25d576a
Azurabeck <rebeccagsouza@gmail.com>
2018-11-27 21:08:43 -0200

    mudando links de acordo com o estado de login

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom'
 import SignInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
+import SignedInLinks from './SignedInLinks';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    
+    const { auth } = props
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+
     return (
         <nav className='nav-wrapper grey darken-3'> 
             <div className='container'>
                 <Link to='/' className='brand-logo'>MarioPlan</Link>
-                <SignInLinks />
-                <SignedOutLinks />
+                {links}
             </div>
         </nav> 
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log(state)
     return {
-
+        auth: state.firebase.auth
     }
 }
 
